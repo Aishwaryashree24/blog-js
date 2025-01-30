3c809cd7cc7b496ef32cd4c1064a62dde88c1ad5
Aishwarya Shree H <aishwarya.h@jmangroup.com>
2025-01-30 13:11:03 +0530

    added add blogs and display titles function

--- back-end/script.js
+++ back-end/script.js
@@ -31,3 +31,39 @@
 
 // //add a new blog
 // blogs.push({ id: 2, title: "title2", description: "content2", views: 0 });
+
+const storedBlogs = localStorage.getItem('blogs');
+const blogs = storedBlogs ? JSON.parse(storedBlogs) : [
+    { id: 1, title: "blog1", content: "content1", views: 0 }
+];
+
+localStorage.setItem('blogs', JSON.stringify(blogs));
+
+const savedblogs = JSON.parse(localStorage.getItem('blogs'));
+const element = document.getElementById('blog-titles');
+element.innerHTML = '';
+
+savedblogs.forEach(blog => {
+    const blogtitle = document.createElement('div');
+    blogtitle.textContent = blog.title;
+    element.appendChild(blogtitle);
+});
+
+const createblog = document.getElementById('create-blog');
+const blogform = document.getElementById('blog-form');
+
+createblog.addEventListener('click', () => {
+    const new_title = document.getElementById('new-title').value;
+    const new_content = document.getElementById('new-content').value;
+
+    const new_blog = { id: blogs.length + 1, title: new_title, content: new_content, views: 0 };
+    blogs.push(new_blog);
+    localStorage.setItem('blogs', JSON.stringify(blogs));
+
+    const blogtitle = document.createElement('div');
+    blogtitle.textContent = new_title;
+    element.appendChild(blogtitle);
+
+    document.getElementById('new-title').value = '';
+    document.getElementById('new-content').value = '';
+});
